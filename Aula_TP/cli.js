54a79f51be6c7f78b6542c4d5da4c66852444d80
LukeTheJava <lucascandidoluiz@outlook.com>
2022-05-20 10:23:29 -0300

    Projeto02 Para A2 Completo

---
+++ Aula_TP/cli.js
@@ -0,0 +1,19 @@
+import chalk from "chalk";
+import pegarArquivo from "./index.js";
+import validarURL from "./http-validacao.js";
+
+const caminho = process.argv;
+
+async function processaTexto(caminhoDoArquivo){
+    const resultado = await pegarArquivo(caminhoDoArquivo[2]);
+    
+    if(caminho[3] === 'validar'){
+        console.log(chalk.yellow('Links Validados'), await validarURL(resultado));
+    }else{
+        console.log(chalk.yellow('Lista de Links'), resultado);
+    }
+    
+    //console.log(chalk.yellow("Lista de Link"), resultado);
+}
+
+processaTexto(caminho);
